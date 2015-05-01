7fd00d156a8462161ffcf6aac8841b499cef086a
Brian Anderson <banderson@mozilla.com>
2015-04-30 21:40:52 -0700

    Set mime types correctly

--- rs/crater-web/main.rs
+++ rs/crater-web/main.rs
@@ -12,6 +12,7 @@ use std::convert::From;
 use std::io::Error as IoError;
 use iron::prelude::*;
 use iron::status;
+use iron::mime::Mime;
 use router::Router;
 use mount::Mount;
 
@@ -30,7 +31,11 @@ fn main() {
 
 fn static_router() -> Router {
     let mut router = Router::new();
-    router.get("/*", move |r: &mut Request| {
+    router.get("/", move |_: &mut Request| {
+        let (payload, mime_type) = try!(get_static_file_and_mime_type("index.html"));
+        Ok(Response::with((status::Ok, payload)).set(mime_type))
+    });
+    router.get("*", move |r: &mut Request| {
         let last = r.url.path.last().expect("path is supposed to be non-empty");
         let filename = if last == "" {
             String::from("index.html")
@@ -38,9 +43,9 @@ fn static_router() -> Router {
             last.clone()
         };
 
-        let payload = try!(get_static_file(&filename));
+        let (payload, mime_type) = try!(get_static_file_and_mime_type(&filename));
         
-        Ok(Response::with((status::Ok, payload)))
+        Ok(Response::with((status::Ok, payload)).set(mime_type))
     });
 
     return router;
@@ -51,10 +56,21 @@ fn api_router() -> Router {
     router.get("/stuff", move |_: &mut Request| {
         println!("test2");
 
-        Ok(Response::with(status::Ok))
+        Ok(Response::with(status::Ok).set(known_mime_type("application/json")))
     });
 
     return router;
+}
+
+fn get_static_file_and_mime_type(name: &str) -> Result<(String, Mime), Error> {
+    let payload = try!(get_static_file(&name));
+    let mime_type = known_mime_type(try!(get_mime_type(&name)));
+
+    return Ok((payload, mime_type));
+}
+
+fn known_mime_type(mime_type: &str) -> Mime {
+    mime_type.parse().ok().expect("shouldn't create mime types that don't parse")
 }
 
 /// Loads a file from the './static' directory
@@ -77,15 +93,29 @@ fn get_static_file(name: &str) -> Result<String, Error> {
     return Ok(s);
 }
 
+fn get_mime_type(name: &str) -> Result<&'static str, Error> {
+    if name.ends_with(".html") {
+        Ok("text/html")
+    } else if name.ends_with(".js") {
+        Ok("application/x-javascript")
+    } else if name.ends_with(".css") {
+        Ok("text/css")
+    } else {
+        Err(Error::BadMimeType)
+    }
+}
+
 #[derive(Debug)]
 pub enum Error {
-    FileNotFound
+    FileNotFound,
+    BadMimeType
 }
 
 impl StdError for Error {
     fn description(&self) -> &str {
         match *self {
-            Error::FileNotFound => "file not found"
+            Error::FileNotFound => "file not found",
+            Error::BadMimeType => "bad mime type"
         }
     }
 }
